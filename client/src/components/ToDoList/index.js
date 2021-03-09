fc8869f5d6b7fb5a2015e27e7c642ebf5bb9573f
Tommy Shi <tommyshi19991130@gmail.com>
2021-03-09 22:44:30 +1300

    Save todo list items to browser local storage

--- client/src/components/ToDoList/index.js
+++ client/src/components/ToDoList/index.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import ToDoItem from "./ToDoItem";
 
-const { useState } = React;
+const saveListToStorage = (list) => {
+    const listJson = JSON.stringify(list);
+    localStorage.setItem("todoList", listJson);
+};
 
 function ToDoList() {
     const [currentValue, setCurrentValue] = useState("");
     const [todoList, setTodoList] = useState([]);
+
+    useEffect(() => {
+        const savedTodoListJson = localStorage.getItem("todoList");
+        if (savedTodoListJson) {
+            const savedTodoList = JSON.parse(savedTodoListJson);
+            setTodoList(savedTodoList);
+        }
+    }, []);
 
     const addNewItem = e => {
         e.preventDefault();
@@ -17,18 +28,21 @@ function ToDoList() {
         const newList = [...todoList, newItemObject];
         setTodoList(newList);
         setCurrentValue("");
+        saveListToStorage(newList);
     };
 
     const deleteItem = index => {
         const newList = [...todoList];
         newList.splice(index, 1);
         setTodoList(newList);
+        saveListToStorage(newList);
     };
 
     const toggleCheck = index => {
         const newList = [...todoList];
         newList[index].checked = !newList[index].checked;
         setTodoList(newList);
+        saveListToStorage(newList);
     };
 
     return (
