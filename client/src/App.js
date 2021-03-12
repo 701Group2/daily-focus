23d57993e0bde5b03a00fee5d56e711089c63034
Sannan Hafeez <sannanhafeez06@gmail.com>
2021-03-13 11:04:23 +1300

    Add empty Header component with temporary placeholder text

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
+
+import Header from "./components/Header";
 import "./style.css";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/components/index.js</code> and save to reload.
-                    <br />
-                    ** DAILY FOCUS **
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Header />
         </div>
     );
 }
