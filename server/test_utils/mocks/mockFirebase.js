cd21673fdc1c62f974d13cc10b50dfe75f9c2010
Jennak21 <12jenna.k@gmail>
2021-03-19 09:19:38 +1300

    created mock firebase for testing

---
+++ server/test_utils/mocks/mockFirebase.js
@@ -0,0 +1,34 @@
+const mockToken = "token";
+const mockUid = "uid";
+
+
+const mockCreateUserWithEmailAndPassword = jest.fn((inputEmail, inputPassword) => {
+  if (inputPassword.length > 5) {
+    return Promise.resolve(mockUserData) 
+  } else {
+    throw new Error("error");
+  }
+})
+
+const mockUserData = {
+  user: {
+    uid: mockUid,
+    getIdToken: jest.fn(() => Promise.resolve(mockToken))
+  } 
+}
+
+const mockAuthObject  = {
+  createUserWithEmailAndPassword: mockCreateUserWithEmailAndPassword
+};
+
+const mockFirebase = {
+  auth: jest.fn(() => (mockAuthObject)),
+  initializeApp: jest.fn(({}) => ({})) 
+
+}
+
+module.exports = mockFirebase;
+
+
+
+
