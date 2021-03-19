65bae4faec5b52751071d348585d4876ea00bffb
Bruce-Gang <73207848+ggao007@users.noreply.github.com>
2021-03-19 20:42:00 +1300

    clear my code on the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
 
 import Header from "./components/Header";
 import "./App.css";
 
-=======
-import logo from "./logo.svg";
-import "./style.css";
-import PlantButton from "./components/PlantTrees/PlantButton/PlantButton";
->>>>>>> Change the class component to the functional component
 function App() {
     return (
         <div className="App">
