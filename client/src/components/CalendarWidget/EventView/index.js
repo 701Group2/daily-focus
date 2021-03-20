59f6aa60d89a8331c5ffe02476a23070b677471f
Michael Pham <tpha585@aucklanduni.ac.nz>
2021-03-20 16:58:45 +1300

    Styling calendar (in progress)

--- client/src/components/CalendarWidget/EventView/index.js
+++ client/src/components/CalendarWidget/EventView/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+// import Button from "@material-ui/core/Button";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import IconButton from "@material-ui/core/IconButton";
 
 const months = [
     "January",
@@ -16,12 +24,15 @@ const months = [
     "December",
 ];
 
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 //evaluates truthy if the two date objects have the same day
 const compareDate = (day1, day2) => {
     return (
         day1.getFullYear() === day2.getFullYear() &&
         day1.getMonth() === day2.getMonth() &&
-        day1.getDate() === day2.getDate()
+        day1.getDate() === day2.getDate() &&
+        day1.getDay() === day2.getDay()
     );
 };
 
@@ -36,28 +47,61 @@ const formatTime = (date) => {
     return hours + ":" + minutes + " " + ampm;
 };
 
+const gridStyle = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        maxWidth: "350px",
+    },
+    demo: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    title: {
+        margin: theme.spacing(4, 0, 2),
+        fontFamily: "'Ropa Sans'",
+    },
+
+    buttonStyle: {
+        background: "#30A0F5",
+        border: 0,
+        borderRadius: 32,
+        color: "white",
+        height: 44,
+        padding: "5px 10px",
+    },
+}));
+
 export default function EventView({ selected, events, onRemoveEvent }) {
+    const gridStyles = gridStyle();
+
     return (
-        <div>
-            <h2>
-                {selected.getDate()} {months[selected.getMonth()]}
-            </h2>
-            <ul>
+        <Grid className={gridStyles.root} item xs={12} md={6}>
+            <Typography variant="h5" className={gridStyles.title}>
+                {days[selected.getDay()]} {selected.getDate()} {months[selected.getMonth()]},{" "}
+                {selected.getFullYear()}
+            </Typography>
+            <List>
                 {events.map((event, index) =>
                     compareDate(selected, event.date) ? (
-                        <div key={index}>
-                            <h3>
+                        <ListItem key={index}>
+                            <Typography variant="h6" className={gridStyles.title}>
                                 {formatTime(event.date)} {event.title}
-                            </h3>
-                            <p>{event.description}</p>
-                            <button onClick={() => onRemoveEvent(events.indexOf(event))}>
-                                Remove Event
-                            </button>
-                        </div>
+                            </Typography>
+                            <Typography variant="h7" className={gridStyles.title}>
+                                {event.description}
+                            </Typography>
+                            <IconButton
+                                className={gridStyles.buttonStyle}
+                                edge="end"
+                                aria-label="delete"
+                                onClick={() => onRemoveEvent(events.indexOf(event))}
+                            >
+                                <DeleteIcon />
+                            </IconButton>
+                        </ListItem>
                     ) : null
                 )}
-            </ul>
-        </div>
+            </List>
+        </Grid>
     );
 }
 
--- client/src/components/CalendarWidget/index.js
+++ client/src/components/CalendarWidget/index.js
@@ -3,6 +3,10 @@ import Calendar from "react-calendar";
 import EventView from "./EventView";
 import NewEvent from "./NewEvent";
 import initialEvents from "./initial-events.js";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import "./Calendar.scss";
 
 const compareDate = (day1, day2) => {
     return (
@@ -12,13 +16,43 @@ const compareDate = (day1, day2) => {
     );
 };
 
+const gridStyle = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        width: "350px",
+        maxWidth: "100%",
+        background: "white",
+        border: "1px solid #a0a096",
+        borderRadius: "20px",
+        boxShadow: "0px 4px 16px 8px rgba(0, 0, 0, 0.25)",
+        padding: "0px",
+    },
+    demo: {
+        backgroundColor: theme.palette.background.paper,
+    },
+
+    circle: {
+        width: "6px",
+        height: "6px",
+    },
+}));
+
 export default function CalendarWidget() {
     const [selected, setSelected] = useState(new Date());
     const [events, setEvents] = useState(initialEvents);
 
+    //Declare grid styling
+    const gridStyles = gridStyle();
+
     //Add content to the calendar tiles
     const tileContent = ({ date }) => (
-        <div>{events.map((event) => (compareDate(event.date, date) ? event.title : null))}</div>
+        <div>
+            {events.map((event) =>
+                compareDate(event.date, date) ? (
+                    <FiberManualRecordIcon className={gridStyles.circle} />
+                ) : null
+            )}
+        </div>
     );
 
     const onAddNewEvent = (title, description) => {
@@ -47,16 +81,14 @@ export default function CalendarWidget() {
     };
 
     return (
-        <div>
-            <div style={{ width: "400px" }}>
+        <Grid className={gridStyles.root}>
+            <Grid>
                 <Calendar onClickDay={(value) => setSelected(value)} tileContent={tileContent} />
-            </div>
-            <div>
+            </Grid>
+            <Grid>
                 <EventView selected={selected} events={events} onRemoveEvent={onRemoveEvent} />
-            </div>
-            <div>
                 <NewEvent onAddNewEvent={onAddNewEvent} />
-            </div>
-        </div>
+            </Grid>
+        </Grid>
     );
 }
