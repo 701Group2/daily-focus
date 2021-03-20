86cbc1ff60c4958ea2e61866d54174fbcfab422b
Bruce-Gang <73207848+ggao007@users.noreply.github.com>
2021-03-20 18:52:02 +1300

    Change to the permanent image link and change some words of feedback

--- client/src/components/PlantButton/stateUtils.js
+++ client/src/components/PlantButton/stateUtils.js
@@ -3,18 +3,19 @@ module.exports = {
         water: "water",
     },
     plantImageUrls: [
-        "https://i.ibb.co/DVP9kkq/Tree1.png",
-        "https://i.ibb.co/CnQgYZp/Tree2.png",
-        "https://i.ibb.co/2s4TDYc/Tree3.png",
-        "https://i.ibb.co/5r5tqWY/Tree4.png",
+        "https://user-images.githubusercontent.com/73207848/111860287-74fbbd00-89ab-11eb-808b-11a7216f1bb5.png",
+        "https://user-images.githubusercontent.com/73207848/111860306-9492e580-89ab-11eb-922a-efa897c1da55.png",
+        "https://user-images.githubusercontent.com/73207848/111860313-99f03000-89ab-11eb-9f60-16eab3880eba.png",
+        "https://user-images.githubusercontent.com/73207848/111860320-a1173e00-89ab-11eb-8843-de4edfa0a383.png",
     ],
     buttonImageUrls: {
-        water: "https://i.ibb.co/C04zg8V/Watering-Can.png",
+        water:
+            "https://user-images.githubusercontent.com/73207848/111860325-a7a5b580-89ab-11eb-936d-bbc974cf89cf.png",
     },
     feedback: {
         start:
-            "Dear, When you finish your task, you will get a Water Coin, you can use this coin to plant your own trees! Try to click the button!",
+            "When you finish a task, you will get water so you can water your tree! Try to click the watering can.",
         win: "Congratulations! You are now a certified plant Mom.",
-        noCoin: "Sorry, You do not have enough coins to water the tree.",
+        noCoin: "Sorry, you need to complete more tasks to water the tree.",
     },
 };
