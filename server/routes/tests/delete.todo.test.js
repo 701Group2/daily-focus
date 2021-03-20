a20002dbb6f36a431cc84ea3f92399fac3661ebf
LincRui <73324110+LincRui@users.noreply.github.com>
2021-03-21 02:49:18 +1300

    Create DELETE API handler for Todo list (#41)

---
+++ server/routes/tests/delete.todo.test.js
@@ -0,0 +1,58 @@
+const request = require("supertest");
+const app = require("../../app");
+
+const expectedAllTodo = [
+    0: {
+        date: "21/03/2021",
+        description: "sleep",
+        entry_id: 1,
+        ticked: false,
+        time: "10:00",
+        title: "Shopping",
+    },
+    1: {
+        date: "21/03/2021",
+        description: "Go shopping with bob",
+        entry_id: 2,
+        ticked: false,
+        time: "08:00",
+        title: "Shopping",
+    },
+];
+
+const expectedAfterDeleteTodo = [
+    0: {
+        date: "21/03/2021",
+        description: "sleep",
+        entry_id: 1,
+        ticked: false,
+        time: "10:00",
+        title: "Shopping",
+    }
+];
+
+jest.mock("./../../firebase", () => {
+    const mockDatabase = require("../../test_utils/mocks/mockDatabase");
+    return {
+        database: mockDatabase,
+    };
+});
+
+jest.mock("../../auth", () => {
+    return jest.fn(() => Promise.resolve("mockValidToken"));
+});
+
+describe("/todo endpoint", () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("Delete a todo item by pass in userId and todoId", async () => {
+        const response = await request(app)
+            .delete("/todo/userId/1/entryId/2")
+            .set("Accept", "application/json");
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(expectedAfterDeleteTodo);
+    });
+});
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,32 +1,57 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
+var database = require('../firebase.js').database;
+const authorise = require("../auth");
+//
 
 /* GET all todo list entries */
-router.get("/", function (req, res, next) {
+router.get('/', function(req, res, next) {
     // TODO implementation for getting entries
 
-    res.send("get all todo entries"); // TODO change response after
+    res.send('get all todo entries'); // TODO change response after
 });
+
 
 /* POST new todo entry */
-router.post("/", function (req, res, next) {
+router.post('/', function(req, res, next) {
     // TODO implementation for adding new entry to database
 
-    res.send("post new entry to database"); // TODO change response after
+    res.send('post new entry to database'); // TODO change response after
 });
+
 
 /* PUT entry update */
-router.put("/", function (req, res, next) {
+router.put('/', function(req, res, next) {
     // TODO implementation for updating entry in database
 
-    res.send("put entry update into database"); // TODO change response after
+    res.send('put entry update into database'); // TODO change response after
 });
 
-/* DELETE todo list entry*/
-router.delete("/", function (req, res, next) {
-    // TODO implementation deleting entry from our database
 
-    res.send("delete todo list entry"); // TODO change response after
+/* DELETE todo list entry*/
+router.delete('/userId/:userId/entryId/:entryId', function(req, res, next) {
+    let userId = authorise(req);
+
+    if (userId === "") {
+        return res.status(401).send("Unauthorised user.");
+    }
+
+    var originData, updatedArray = [];
+    
+    database.ref(req.params.userId + '/todolist/').get().then(function(snapshot) {
+        if (snapshot.exists()) {
+            originData = snapshot.val();
+            updatedArray = originData.filter(entry => entry.entry_id != req.params.entryId);
+            database.ref(req.params.userId + '/todolist').set(updatedArray);
+
+            res.status(200).send(updatedArray);
+        }
+        else {
+          console.log("No data available");
+        }
+    }).catch(function(error) {
+        console.error(error);
+    });
 });
 
 module.exports = router;
