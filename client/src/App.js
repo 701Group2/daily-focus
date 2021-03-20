e592c26119ec1091d6a36cec3677eb5d9a598fcf
HenderikusP <henderikus.parekowhai@gmail.com>
2021-03-20 18:14:13 +1300

    Imported empty component into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import Header from "./components/Header";
+import Timer from "./components/Timer";
 import "./App.css";
+import { Timer } from "@material-ui/icons";
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Timer />
         </div>
     );
 }
