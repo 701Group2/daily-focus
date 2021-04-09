0eb0f91c70222d5930d2301594654c9d0129483b
M-Stable <fchanghlee@gmail.com>
2021-04-09 15:04:25 +1200

    Spotify widget in manage widgets

--- client/src/components/SelectWidgetsModal/index.js
+++ client/src/components/SelectWidgetsModal/index.js
@@ -67,6 +67,12 @@ function SelectWidgetsModal({ selectedWidgets, setSelectedWidgets }) {
                             selectedWidgets={selectedWidgets}
                             setSelectedWidgets={setSelectedWidgets}
                         />
+                        <WidgetIcon
+                            image={timer}
+                            label="Spotify"
+                            selectedWidgets={selectedWidgets}
+                            setSelectedWidgets={setSelectedWidgets}
+                        />
                     </Grid>
                 </DialogContent>
             </Dialog>
