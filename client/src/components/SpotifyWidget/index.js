ff56d95dc1d2977251c882843373dd8cefb7c9b1
M-Stable <fchanghlee@gmail.com>
2021-04-09 15:53:08 +1200

    implement spotify login and playback controles

--- client/src/components/SpotifyWidget/index.js
+++ client/src/components/SpotifyWidget/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
@@ -7,6 +7,10 @@ import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import FastForwardIcon from "@material-ui/icons/FastForward";
 import FastRewindIcon from "@material-ui/icons/FastRewind";
 import PauseIcon from "@material-ui/icons/Pause";
+import { accessUrl, getTokenFromResponse } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 const useStyles = makeStyles({
     root: {
@@ -26,6 +30,64 @@ export default function SpotifyWidget() {
     const classes = useStyles();
     const [play, setPlay] = useState(false);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [item, setItem] = useState(false);
+
+    useEffect(() => {
+        const hash = getTokenFromResponse();
+        window.location.hash = "";
+        const _token = hash.access_token;
+
+        if (_token) {
+            spotify.setAccessToken(_token);
+            setIsLoggedIn(true);
+
+            //not needed but just logging to show user info
+            spotify.getMe().then((user) => {
+                console.log(user);
+            });
+
+            spotify.getMyCurrentPlaybackState().then((track) => {
+                setPlay(track.is_playing);
+            });
+        }
+    }, []);
+
+    const handlePlay = () => {
+        spotify.getMyDevices().then((devices) => {
+            if (play) {
+                spotify.pause();
+            } else {
+                spotify
+                    .play({
+                        device_id: devices.devices[0]?.id,
+                        context_uri: "spotify:playlist:0vvXsWCC9xrXsKd4FyS8kM",
+                    })
+                    .then(() => {
+                        spotify.getMyCurrentPlayingTrack().then((r) => {
+                            setItem(r.item);
+                        });
+                    });
+            }
+        });
+    };
+
+    const skipNext = () => {
+        spotify.skipToNext().then(() => {
+            spotify.getMyCurrentPlayingTrack().then((r) => {
+                setItem(r.item);
+                setPlay(true);
+            });
+        });
+    };
+
+    const skipPrevious = () => {
+        spotify.skipToPrevious().then(() => {
+            spotify.getMyCurrentPlayingTrack().then((r) => {
+                setItem(r.item);
+                setPlay(true);
+            });
+        });
+    };
 
     return (
         <div className={style.spotify}>
@@ -34,9 +96,7 @@ export default function SpotifyWidget() {
             </div>
             {!isLoggedIn ? (
                 <Button
-                    onClick={() => {
-                        setIsLoggedIn(true);
-                    }}
+                    href={accessUrl}
                     variant="contained"
                     color="primary"
                     classes={{ root: classes.root, label: classes.label }}
@@ -45,16 +105,20 @@ export default function SpotifyWidget() {
                 </Button>
             ) : (
                 <div className={style.spotifyBody}>
-                    {/* replace this div with an image of the song or playlist */}
-                    <div className={style.songImage} />
+                    <img
+                        className={style.songImage}
+                        src={item?.album?.images[0]?.url}
+                        alt={item?.album?.name}
+                    />
                     <div>
-                        <IconButton aria-label="rewind">
+                        <IconButton aria-label="rewind" onClick={skipPrevious}>
                             <FastRewindIcon fontSize="large" />
                         </IconButton>
                         <IconButton
                             aria-label="play"
                             onClick={() => {
                                 setPlay(!play);
+                                handlePlay();
                             }}
                         >
                             {play ? (
@@ -63,7 +127,7 @@ export default function SpotifyWidget() {
                                 <PlayArrowIcon fontSize="large" />
                             )}
                         </IconButton>
-                        <IconButton aria-label="fastforward">
+                        <IconButton aria-label="fastforward" onClick={skipNext}>
                             <FastForwardIcon fontSize="large" />
                         </IconButton>
                     </div>
